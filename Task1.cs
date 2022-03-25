79cb959651cb13cd05a1339d1dbfc507dcd7b6d2
Kirya052 <kiryakonev01@mail.ru>
2022-03-25 19:36:52 +0500

    Add Task1 solution

---
+++ Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write string:");
+            string str = Console.ReadLine();
+
+            var builder = new StringBuilder();
+
+            str = str.ToLower();
+
+            for (int i = 0; i<str.Length; i++)
+            {
+                if(str.LastIndexOf(str[i]) != str.IndexOf(str[i]))
+                {
+                    builder.Append(")");
+                }
+                else
+                {
+                    builder.Append("(");
+                }
+            }
+
+            Console.WriteLine(builder);
+            Console.ReadKey();
+        }
+    }
+}
